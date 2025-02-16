11d2ef4c5cefa79f6eecfbc9b5f2e497f87f1418
manjotsk0850 <94953471+manjotsk0850@users.noreply.github.com>
2025-02-16 23:48:26 +0530

    Add files via upload

---
+++ L0 - Square and Cube of a Number with Implicit Return.js
@@ -0,0 +1,3 @@
+const squareAndCube = (num) => ({ square: num ** 2, cube: num ** 3 });
+
+console.log(squareAndCube(5));
---
+++ L0 - Understanding Global and Function Execution Context.js
@@ -0,0 +1,13 @@
+let age = 27;
+
+function displayAge(age){
+  console.log(age);
+}
+
+function changeAge(age){
+  age = age - 5
+  console.log(age);
+}
+
+displayAge(age);
+changeAge(age);
